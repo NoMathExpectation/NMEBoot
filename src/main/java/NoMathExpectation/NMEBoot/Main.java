d5fcf74e2a35b37c404101ac113de2c7ce95915b
NoMathExpectation <85624722+NoMathExpectation@users.noreply.github.com>
2022-04-27 22:59:47 +0800

    minor update

--- src/main/java/NoMathExpectation/NMEBoot/Main.java
+++ src/main/java/NoMathExpectation/NMEBoot/Main.java
@@ -21,7 +21,7 @@ public final class Main extends JavaPlugin {
     public static WordleMirai wordle = new WordleMirai(new File("config/NoMathExpectation.NMEBoot/wordle.txt"), 6, 25);
 
     private Main() {
-        super(new JvmPluginDescriptionBuilder("NoMathExpectation.NMEBoot", "1.1.34-2022042701")
+        super(new JvmPluginDescriptionBuilder("NoMathExpectation.NMEBoot", "1.1.34-2022042702")
                 .name("NMEBoot")
                 .author("NoMathExpectation")
                 .build());
@@ -107,7 +107,7 @@ public final class Main extends JavaPlugin {
         //注册监听
         GlobalEventChannel.INSTANCE.registerListenerHost(executeCenter);
 
-        new Thread(Subscribe.INSTANCE::subscribeChatRank).start();
+        Subscribe.INSTANCE.subscribeChatRank();
 
         //自动保存
         Thread t = new Thread(this::autoSave);
--- src/main/java/NoMathExpectation/NMEBoot/RDLounge/Subscribe.kt
+++ src/main/java/NoMathExpectation/NMEBoot/RDLounge/Subscribe.kt
@@ -1,28 +1,26 @@
 package NoMathExpectation.NMEBoot.RDLounge
 
+import NoMathExpectation.NMEBoot.Main
 import NoMathExpectation.NMEBoot.commandSystem.NormalUserStats
 import NoMathExpectation.NMEBoot.commandSystem.RDLounge
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.isActive
 import kotlinx.coroutines.launch
-import kotlinx.coroutines.runBlocking
 import net.mamoe.mirai.Bot
 import java.time.LocalDateTime
 
 object Subscribe {
     private const val id = RDLounge.GROUP_ID
 
-    fun subscribeChatRank() = runBlocking {
-        launch {
-            while (isActive) {
-                val time = LocalDateTime.now()
-                if (time.hour == 23 && time.minute == 59) {
-                    val contact = Bot.instances[0].getGroup(id) ?: continue
-                    contact.sendMessage(NormalUserStats.getMessageCountAndDailyAsForwardMessage(contact))
-                    delay(60000L)
-                }
-                delay(10000L)
+    fun subscribeChatRank() = Main.INSTANCE.launch {
+        while (isActive) {
+            val time = LocalDateTime.now()
+            if (time.hour == 23 && time.minute == 59) {
+                val contact = Bot.instances[0].getGroup(id) ?: continue
+                contact.sendMessage(NormalUserStats.getMessageCountAndDailyAsForwardMessage(contact))
+                delay(60000L)
             }
+            delay(10000L)
         }
     }
 }
